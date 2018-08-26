9643e995240441f6719e52fd7da4410dd7531259
kulabuha <kulabuha@inbox.ru>
2018-08-26 21:51:34 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j.calculate;
-
+/**
+* Calculate. Вывод строки "Hello World!".
+*
+* @author Alexey Kulabuhov (mailto:jkulabuha@yandex.ru)
+*/
 public class Calculate {
+	/**
+	*Main.
+	*@param args - args.
+	*/
 	public static void main (String[]args) {
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexey Kulabuhov (mailto:jkulabuha@yandex.ru)
+* @version 1.0
+* @since 26.08.2018
+*/
+package ru.job4j.calculate;

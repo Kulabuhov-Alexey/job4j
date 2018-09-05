8debfbca890b7fd982d9b898eda0a435348df1f5
kulabuha <kulabuha@inbox.ru>
2018-09-05 18:43:25 +0300

    5.2. Создать программу вычисляющую факториал. [#193] убрал ненужное условие из цикла

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -11,9 +11,6 @@ public class Factorial {
     public int calc(int n) {
         int factorial = 1;
         for (int i = 1; i <= n; i++) {
-            if (n == 0) {
-                break;
-            }
             factorial *= i;
         }
         return  factorial;

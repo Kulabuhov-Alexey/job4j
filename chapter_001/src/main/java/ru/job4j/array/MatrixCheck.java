1a357d47710b34028210b584710874bc5b344e9d
kulabuha <kulabuha@inbox.ru>
2018-09-17 21:22:24 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям. [#53859] - добавил новый метод, теперь проверяет "четные" матрицы согласно условию задачи.

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -9,17 +9,37 @@ public class MatrixCheck {
      * @return все диагонали содержат одинаковые элементы.
      */
     public boolean mono(boolean[][] data) {
+        boolean result;
+        if (data.length % 2 == 0) {
+            result = check(data, 'l') || check(data, 'r');
+        } else {
+            result = check(data, 'l') && check(data, 'r');
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param data массив где проверяем диагонали.
+     * @param d какую диагональ проверяем (левую или правую).
+     * @return совпадают ли элементы в проверяемой диагонали.
+     */
+    public boolean check(boolean[][] data, char d) {
         boolean result = true;
-        boolean tmp = data[0][0];
-        if (data.length > 2) {
-            for (int i = 0; i < data.length; i++) {
-                if ((tmp != data[i][i]) || (tmp != data[data.length - 1 - i][i])) {
+        if (d == 'l') {
+            for (int i = 1; i < data.length; i++) {
+                if (data[0][0] != data[i][i]) {
                     result = false;
                 }
             }
-        } else if (data[0][0] != data[1][1] && data[1][0] != data[0][1]) {
-            result = false;
+        } else if (d == 'r') {
+            for (int i = 1; i < data.length; i++) {
+                if (data[0][data.length - 1] != data[data.length - 1 - i][i]) {
+                    result = false;
+                }
+            }
         }
+
         return result;
     }
 }
